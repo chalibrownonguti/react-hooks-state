fc510ea7357a75aff2ee8019e8f05c551d07e57d
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-05 16:43:04 +0300

    success

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,11 +4,8 @@ function Counter() {
   const [count, setCount] = useState(0);
   
   function increment() {
-     console.log(`before setState: ${count}`);
-
+     setCount(count + 1);
     setCount(count + 1);
-
-    console.log(`after setState: ${count}`);
   }
 
   return <div onClick={increment}>{count}</div>;
